a36d12526963bac9c1a81172af545b6ffd040ad0
Chimary7 <jazmin.melo@usach.cl>
2024-12-28 16:37:28 -0300

    feat: create aesthetic login front

---
+++ grupo2Frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '9/10': '95%',
+        '48r' : '48rem',
+      },
+
+      colors: {
+        'color-primary': '#EDE7F6',
+        'color-secondary': '#D1C4E9',
+        'color-tertiary': '#B39DDB',
+        'color-quaternary': '#9575CD',
+        'color-quinary': '#512DA8',
+      },
+    },
+  },
+  plugins: [],
+}
