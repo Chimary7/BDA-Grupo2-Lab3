2509c2c84d6f94a6a5c7e0a8f67d990210617dd2
Chimary7 <jazmin.melo@usach.cl>
2024-12-29 17:50:31 -0300

    refactor: AllProduct structure

--- grupo2Frontend/src/components/Client/AllProductsClient.vue
+++ grupo2Frontend/src/components/Client/AllProductsClient.vue
@@ -4,8 +4,11 @@
 
 <template>
     <div class="h-screen w-full flex items-center justify-center">
-        <div class="h-1/2 w-1/2 flex items-center justify-center flex-col">
+        <div class="h-full w-1/6 flex items-center justify-center flex-col bg-blue-400">
             
         </div>
+        <section class="w-5/6 h-full bg-red-400">
+
+        </section>
     </div>
 </template>
--- grupo2Frontend/src/router.js
+++ grupo2Frontend/src/router.js
@@ -3,7 +3,6 @@ import Home from './components/client/HomeClient.vue';
 import Register from './components/client/Register.vue';
 import Login from './components/client/Login.vue';
 import AllProducts from './components/client/AllProductsClient.vue';
-import AllProducts from './components/client/AllProductsClient.vue';
 import Admin from './components/Admin/HomeAdmin.vue';
 import CrearProducto from './components/Admin/CrearProducto.vue';
 import OrdenPorCliente from './components/Admin/OrdenPorCliente.vue';
