9a3f7386c55467dc68f1f64b0a2e549e20e8e4c7
Chimary7 <jazmin.melo@usach.cl>
2025-01-01 19:13:52 -0300

    feat: create CRUD archivosProducto

---
+++ grupo2backend/src/main/java/com/example/grupo2backend/Services/ArchivosProductosService.java
@@ -0,0 +1,54 @@
+package com.example.grupo2backend.Services;
+
+import com.example.grupo2backend.Entities.ArchivosProducto;
+import com.example.grupo2backend.Repositories.ArchivosProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArchivosProductosService {
+    @Autowired
+    private ArchivosProductoRepository archivosProductoRepository;
+
+    public Boolean saveArchivoProducto(ArchivosProducto archivoProducto) {
+        try {
+            archivosProductoRepository.save(archivoProducto);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public List<ArchivosProducto> findAll() {
+        return archivosProductoRepository.findAll();
+    }
+
+    public ArchivosProducto findArchivoProductoById(String id) {
+        return archivosProductoRepository.findById(id).orElse(null);
+    }
+
+    public List<ArchivosProducto> findArchivoProductoByIdProducto(String idProducto) {
+        return archivosProductoRepository.findByIdProducto(idProducto);
+    }
+
+    public Boolean updateArchivoProducto(ArchivosProducto archivoProducto) {
+        try {
+            archivosProductoRepository.save(archivoProducto);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Boolean deleteArchivoProductoById(String id) {
+        try {
+            archivosProductoRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
